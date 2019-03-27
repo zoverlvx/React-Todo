332928e6475f6a4a8d1fd062eda8e34af140e275
zoverlvx <zoverhulser@gmail.com>
2019-03-27 16:20:09 -0400

    Creates unordered list for items to be displayed in.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,11 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import Todo from "./Todo";
+
+const TodoList = ({todos}) => {
+    return (
+        <ul>
+            {todos.map(({task}) => <Todo item={task} />)}
+        </ul>
+    )
+}
